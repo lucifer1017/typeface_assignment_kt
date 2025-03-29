bdbf1d4647a1803c22f5fd6e51c50b6b4d966aa4
lucifer1017 <lucifergrey1017@gmail.com>
2025-03-29 20:11:45 +0530

    DB setup, app.js setup

---
+++ server/src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const dotenv = require('dotenv');
+const { connectToDB } = require('./config/database');
+dotenv.config();
+
+app.use(express.json());
+app.use(cors({
+    origin: process.env.BASE_URL,
+    credentials: true
+}));
+const port = process.env.PORT || 5000;
+
+connectToDB()
+    .then(() => {
+        console.log("Connected to DB successfully");
+        app.listen(port, () => {
+            console.log(`Server running successfully on PORT:${port}`);
+        })
+    })
+    .catch((err) => {
+        console.error("Cannot establish connection");
+    })
---
+++ server/src/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const connectToDB = async () => {
+    const mongoUri = process.env.MONGO_URI;
+    if (!mongoUri)
+        throw new Error("Connection to DB not successful, kindly check connection string");
+
+    await mongoose.connect(mongoUri);
+}
+
+module.exports = { connectToDB };
